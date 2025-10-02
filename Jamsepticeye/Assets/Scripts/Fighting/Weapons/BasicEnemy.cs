2fa20a3e756595cdc6dc8f74843dcc041f82db02
You.Ran.Wang <thatrandomguy666666@gmail.com>
2025-10-02 15:25:31 -0400

    Add enemy movement.
    Should proabably add NULL checks in the future.

--- Jamsepticeye/Assets/Scripts/Fighting/Weapons/BasicEnemy.cs
+++ Jamsepticeye/Assets/Scripts/Fighting/Weapons/BasicEnemy.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class BasicEnemy : MonoBehaviour
 {
     [SerializeField] private int hp = 10;
+    [SerializeField] private float speed = 2f;
 
     private Rigidbody2D rb;
+    [SerializeField] private Transform player;
 
     void Awake()
     {
@@ -18,7 +20,8 @@ public class BasicEnemy : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
     }
 
     void Update()
