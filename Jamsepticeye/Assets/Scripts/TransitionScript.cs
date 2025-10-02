1509fbaabb7fc519c39858d017e4ea9bfebcba9d
SavageWhiskass <baggottjaiden@gmail.com>
2025-10-02 23:12:06 +0100

    Level transition

---
+++ Jamsepticeye/Assets/Scripts/TransitionScript.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScript : MonoBehaviour
+{
+
+    public int SceneBuildIndex;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Trigger Entered");
+
+        if (other.tag == "Player")
+        {
+            print("Switching scene to " + SceneBuildIndex);
+            SceneManager.LoadScene(SceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+
+
+
+
+
+}
